b5ad2ce8def76dc2c35f8d89cbd5023fed5571c4
Teresa Ferreira <61392994+titf512@users.noreply.github.com>
2026-03-26 20:14:44 +0000

    Merge pull request #3 from titf512/feature/parser
    Feature/parser

--- src/firds/extractor.py
+++ src/firds/extractor.py
@@ -1,6 +1,7 @@
 """Module responsible for downloading files from ESMA."""
 import logging
 import os
+import zipfile
 
 import requests
 
@@ -14,7 +15,7 @@ class Extractor:
         """Downloads the file to the specified path."""
         os.makedirs(os.path.dirname(target_path), exist_ok=True)
 
-        logger.info(f"Initiating download from: {url}")
+        logger.info(f"Initiating download from: {url}\n")
 
         with requests.get(url, stream=True) as r:
             r.raise_for_status()
@@ -22,5 +23,14 @@ class Extractor:
                 for chunk in r.iter_content(chunk_size=8192):
                     f.write(chunk)
 
-        logger.info(f"File successfully saved to: {target_path}")
+        logger.info(f"File successfully saved to: {target_path}\n")
+
+
+
+    def extract_zip(self, zip_path: str, extract_to: str):
+        """Unzips the file and returns the path to the extracted XML."""
+        with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+            zip_ref.extractall(extract_to)
+            xml_filename = zip_ref.namelist()[0]
+            return os.path.join(extract_to, xml_filename)
 
